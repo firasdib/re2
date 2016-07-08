4464546c1f5823f462f63199b5e077791c343cd2
Firas Dib <firas.dib@gmail.com>
2016-07-08 13:24:08 +0200

    Improve the re2 bindings to accept a cached regex

--- re2bindings.cpp
+++ re2bindings.cpp
@@ -1,12 +1,15 @@
 #include <emscripten/bind.h>
 #include <emscripten/val.h>
 #include "re2/re2.h"
+// #include "util/flags.h"
 
 using std::vector;
 using re2::RE2;
 using re2::StringPiece;
 
 using namespace emscripten;
+
+// DECLARE_bool(trace_re2);
 
 class RegexMatch {
   public:
@@ -25,16 +28,22 @@ RegexMatch::RegexMatch (int a, int b) {
   end = b;
 }
 
-std::vector<RegexMatch> RE2_Match(int regex_pointer, int text_pointer, int startpos, int endpos, int captureGroupCount) {
+int RE2_CreateRegex(int regex_pointer) {
   const char* regex = (char*)regex_pointer;
-  const char* match_text = (char*)text_pointer;
 
-  RE2 re(regex);
+  return (int)(new RE2(regex));
+}
+
+std::vector<RegexMatch> RE2_Match(int regex_pointer, int text_pointer, int startpos, int endpos, int captureGroupCount) {
+  // Debug only!
+  // re2::FLAGS_trace_re2 = true;
+
+  const char* match_text = (char*)text_pointer;
 
   std::vector<StringPiece> groups(captureGroupCount + 1);
   std::vector<RegexMatch> result(captureGroupCount + 1);
 
-  bool did_match = re.Match(match_text, startpos, endpos, RE2::UNANCHORED, &groups[0], groups.size());
+  bool did_match = ((RE2*)regex_pointer)->Match(match_text, startpos, endpos, RE2::UNANCHORED, &groups[0], groups.size());
 
   if (did_match) {
     for (size_t i = 0, n = groups.size(); i < n; ++i) {
@@ -57,53 +66,11 @@ std::vector<RegexMatch> RE2_Match(int regex_pointer, int text_pointer, int start
 EMSCRIPTEN_BINDINGS(my_module) {
   register_vector<RegexMatch>("VectorRegexMatch");
 
-  // this may or may not work, let's try directly binding the static funcs
   function("RE2_Match", &RE2_Match);
+  function("RE2_CreateRegex", &RE2_CreateRegex);
 
   class_<RegexMatch>("RegexMatch")
     .constructor<int,int>()
     .function("getStart", &RegexMatch::getStart)
     .function("getEnd", &RegexMatch::getEnd);
-}
-
-// utilities
-
-inline size_t getUtf8Length(const uint16_t* from, const uint16_t* to) {
-  size_t n = 0;
-  while (from != to) {
-    uint16_t ch = *from++;
-    if (ch <= 0x7F) ++n;
-    else if (ch <= 0x7FF) n += 2;
-    else if (0xD800 <= ch && ch <= 0xDFFF) n += 4;
-    else if (ch < 0xFFFF) n += 3;
-    else n += 4;
-  }
-  return n;
-}
-
-inline size_t getUtf16Length(const char* from, const char* to) {
-  size_t n = 0;
-  while (from != to) {
-    unsigned ch = *from & 0xFF;
-    if (ch < 0xF0) {
-      if (ch < 0x80) {
-        ++from;
-      } else {
-        if (ch < 0xE0) {
-          from += 2;
-        } else {
-          from += 3;
-        }
-      }
-      ++n;
-    } else {
-      from += 4;
-      n += 2;
-    }
-  }
-  return n;
-}
-
-inline size_t getUtf8CharSize(char ch) {
-  return ((0xE5000000 >> ((ch >> 3) & 0x1E)) & 3) + 1;
 }
